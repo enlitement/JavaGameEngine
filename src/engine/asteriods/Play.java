31141d29e16330ebf7eb99e8e435e7c776180007
Helson T <helsont@gmail.com>
2012-11-23 14:52:32 -0500

    commit

--- src/engine/asteriods/Play.java
+++ src/engine/asteriods/Play.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
